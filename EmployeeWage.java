d99fd3974fb5ebbeb1826dd24e518489d73a03e7
balu-pinisetty <balupinisetty@gmail.com>
2021-06-08 23:54:20 +0530

    Added: Object method to display Welcome Message

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,15 @@
 package com.bridzlab.employeewageoops;
 
 public class EmployeeWage {
-
+	public static void main(String[] args) {
+		EmployeeWage employeeWageObj = new EmployeeWage();
+		employeeWageObj.welcomeDisplay();
+	}
+	
+	/**
+	 * Displaying Welcome Message.
+	 */
+	private void welcomeDisplay() {
+		System.out.println("Welcome to Employee Wage Computation Problem\n");
+	}
 }
