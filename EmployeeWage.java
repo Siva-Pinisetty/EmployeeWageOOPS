18ba52476695ec73e7fd34d1575f32118eb9af41
balu-pinisetty <balupinisetty@gmail.com>
2021-06-09 00:15:43 +0530

    Added: Object method for Checking Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,12 @@
 package com.bridzlab.employeewageoops;
-
+import java.util.Random;
 public class EmployeeWage {
+	static Random random = new Random();
+	public static final int EMPLOYEE_PRESENT = 1;
 	public static void main(String[] args) {
 		EmployeeWage employeeWageObj = new EmployeeWage();
 		employeeWageObj.welcomeDisplay();
+		employeeWageObj.employeeAttendance();
 	}
 	
 	/**
@@ -11,5 +14,18 @@ public class EmployeeWage {
 	 */
 	private void welcomeDisplay() {
 		System.out.println("Welcome to Employee Wage Computation Problem\n");
+	}
+	
+	/**
+	 * Checking if Employee is present or absent by calling random
+	 */
+	public void employeeAttendance() {
+		int randm = random.nextInt(2);
+		if (randm == EMPLOYEE_PRESENT) {
+			System.out.println("Employee is Present");
+		}	
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
