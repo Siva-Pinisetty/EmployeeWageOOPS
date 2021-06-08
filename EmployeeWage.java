a1f15f94e469cdadfafd6ad12b4e5d38bb65964d
balu-pinisetty <balupinisetty@gmail.com>
2021-06-09 01:43:53 +0530

    Added: Object method for Calculating Monthly Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,13 +2,12 @@ package com.bridzlab.employeewageoops;
 import java.util.Random;
 public class EmployeeWage {
 	static Random random = new Random();
-	public static final int EMPLOYEE_PRESENT = 1, WAGE_PER_HOUR = 20, EMPLOYEE_PART_TIME=2;
-	public static int empworkhours, dailywage;
+	public static final int EMPLOYEE_PRESENT = 1, WAGE_PER_HOUR = 20, EMPLOYEE_PART_TIME=2, MONTH_DAYS=20;
+	public static int empworkhours, dailywage, monthlywage=0;
+	static EmployeeWage employeeWageObj = new EmployeeWage();
 	public static void main(String[] args) {
-		EmployeeWage employeeWageObj = new EmployeeWage();
 		employeeWageObj.welcomeDisplay();
-		employeeWageObj.employeeAttendance();
-		employeeWageObj.dailyWage(empworkhours,WAGE_PER_HOUR);
+		employeeWageObj.monthlyWage();
 	}
 	
 	/**
@@ -26,17 +25,17 @@ public class EmployeeWage {
 		int randm = random.nextInt(3);
 		switch (randm) {
 			case EMPLOYEE_PRESENT: {
-				System.out.println("Employee is Present\n");
+				//System.out.println("Employee is Present\n");
 				empworkhours=8;
 				break;
 			}	
 			case EMPLOYEE_PART_TIME: {
-				System.out.println("Employee is Part-time Present\n");
+				//System.out.println("Employee is Part-time Present\n");
 				empworkhours=4;
 				break;
 			}	
 			default: {
-				System.out.println("Employee is Absent\n");
+				//System.out.println("Employee is Absent\n");
 				empworkhours=0;
 				}
 		}
@@ -48,6 +47,19 @@ public class EmployeeWage {
 	 */
 	public void dailyWage(int empworkhours, int WAGE_PER_HOUR) {
 		dailywage = (empworkhours * WAGE_PER_HOUR);
-		System.out.println(dailywage+" is the Employee daily wage");
+		//System.out.println(dailywage+" is the Employee daily wage\n");
+	}
+	
+	/**
+	 * Calculating monthly employee wage
+	 * @param dailywage, MONTH_DAYS
+	 */
+	public void monthlyWage() {
+		for (int i=1;i<=MONTH_DAYS;i++) {
+			employeeWageObj.employeeAttendance();
+			employeeWageObj.dailyWage(empworkhours,WAGE_PER_HOUR);
+			monthlywage+=dailywage;
+		}
+		System.out.println(monthlywage+" is the Employee monthly wage\n");
 	}
 }
